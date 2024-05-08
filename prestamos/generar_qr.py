82e96e31ca0dfabf3eadd6412b71e390b1b2c0f3
Martin Rey <m.rey00@gmail.com>
2024-05-08 15:55:29 -0300

    Planteo básico de trabajo para la generación de código QR por libro. Relacionado al issue #1

---
+++ prestamos/generar_qr.py
@@ -0,0 +1,13 @@
+# este es el archvi  que te genera un QR para cada libro
+def crear_qr(url):
+    return None
+
+def guardar_img(img):
+    pass
+
+libro = "asfñjasfjñnasfñnsafn"
+url = "biblioteca.apostoles.ar/"
+enalce_qr = f"{url}{libro}"
+
+img = crear_qr(enalce_qr)
+guardar_img(img)
